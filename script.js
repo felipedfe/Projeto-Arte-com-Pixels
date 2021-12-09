4ea746b8490ec7c6da2af267c6c74a30b1ff9b40
Felipe Fernandes <felipedfe@gmail.com>
2021-12-09 16:53:33 -0300

    requisitos 5 a 7

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
 
+  //Adicionando o quadro de pixels
   let linhas = document.getElementsByClassName("linhas");
   
   for (let c = 0; c < linhas.length; c += 1) {
@@ -9,9 +10,24 @@ window.onload = function () {
     }
   }
 
+  //Selecionando as cores da paleta
+  function selecao (evento) {
+    let elementoClicado = evento.target;
+    for (let i = 0; i < cores.length; i += 1) {
+      cores[i].className = "color"
+    }
+    if (elementoClicado.classList[1] !== "selected") {
+      elementoClicado.classList.add("selected");
+    }
+  }
 
+  let cores = document.getElementsByClassName("color");
+  
+  for (let i = 0; i < cores.length; i += 1) {
+    cores[i].addEventListener("click", selecao);
+  }
 
-
+  //
 
 
 
